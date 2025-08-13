59143f3ca241a505ad3409defb7414806e88102f
emalhaolautaro <emalhao.142047@alu.uns.edu.ar>
2025-08-13 18:49:51 -0300

    animacion muerte de raton

--- enemigo.gd
+++ enemigo.gd
@@ -1,19 +1,30 @@
 extends CharacterBody2D
 
-@onready var speed = 250
+@onready var speed = 150
 @onready var lives = 5
+@onready var sprite = null
 var target
 
 signal enemigo_muerto
+
+func _ready():
+	sprite = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	if target:
 		var direction = (target.global_position - global_position).normalized()
 		velocity = direction * speed
+		if direction.x < 0:
+			sprite.flip_h = true
+		sprite.play("move")
 		move_and_slide()
 
 func recibir_daño():
 	lives-=1
 	if lives<=0:
+		target = false
+		sprite.play("death")
+		await get_tree().process_frame  # Espera un frame para mostrar el cambio
+		await get_tree().create_timer(1.0).timeout
 		enemigo_muerto.emit()
 		queue_free()
--- jugador.gd
+++ jugador.gd
@@ -23,13 +23,7 @@ func _physics_process(delta: float) -> void:
 			sprite.play("run")
 		else:
 			velocity = direction * walk_speed
-			if direction == Vector2.UP:
-				sprite.play("atras")
-			elif direction == Vector2.DOWN:
-				sprite.play("frente")
-			else:
-				sprite.play("move")
-			
+			sprite.play("move")
 		
 		# Solo cambiar flip cuando hay input horizontal
 		if direction.x != 0:
