07f7e8a3e4a5c62f1cae1961d216a9d4a0945019
emalhaolautaro <emalhao.142047@alu.uns.edu.ar>
2025-08-12 23:08:37 -0300

    El arma sigue el mouse y dispara desde la punta de esta

--- jugador.gd
+++ jugador.gd
@@ -7,9 +7,6 @@ func _ready():
 	sprite = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
-	if (Input.is_action_just_pressed("disparar")):
-		disparar()
-	
 	var direction = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
 	if direction == Vector2.ZERO:
 		sprite.play("still")
@@ -22,10 +19,3 @@ func _physics_process(delta: float) -> void:
 			
 	velocity = direction*speed
 	move_and_slide()
-
-func disparar():
-	const PROYECTIL = preload("res://proyectil.tscn")
-	var disparo = PROYECTIL.instantiate()
-	disparo.global_position = global_position
-	get_parent().add_child(disparo)
-	disparo.look_at(get_global_mouse_position())
--- lanzaovillos.gd
+++ lanzaovillos.gd
@@ -1,15 +1,36 @@
 extends Sprite2D
 
-func _physics_process(delta: float) -> void:
-	var jugador = $"../AnimatedSprite2D"
-	var direction = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
-	var tex = jugador.sprite_frames.get_frame_texture(jugador.animation, jugador.frame)
-	if direction != Vector2.ZERO:
-		if direction.x > 0:
-			flip_h = false
-			global_position.x = jugador.global_position.x + tex.get_width()/2 + 30
-		elif direction.x < 0: 
-			flip_h = true
-			global_position.x = jugador.global_position.x - tex.get_width()/2 - 30
-		else:
-			global_position.x = jugador.global_position.x
+const MAX_DISTANCE := 50
+@onready var puntaArma = $PuntaArma
+
+func _physics_process(delta):
+	var jugador = get_parent()
+	var global_mouse_pos = get_global_mouse_position()
+	var jugador_pos = jugador.global_position
+
+	var dir = global_mouse_pos - jugador_pos
+
+	if dir.length() > MAX_DISTANCE:
+		dir = dir.normalized() * MAX_DISTANCE
+
+	global_position = jugador_pos + dir
+	rotation = dir.angle()
+
+	flip_v = dir.x < 0
+
+func disparar():
+	const PROYECTIL = preload("res://proyectil.tscn")
+	var disparo = PROYECTIL.instantiate()
+
+	# Posicionar el disparo justo en la punta
+	disparo.global_position = puntaArma.global_position
+
+	# Rotar disparo para que apunte al mouse
+	disparo.rotation = (get_global_mouse_position() - puntaArma.global_position).angle()
+
+	# Agregar el proyectil a la escena (hermano o padre, según cómo esté tu jerarquía)
+	get_tree().current_scene.add_child(disparo)
+	
+func _process(delta):
+	if Input.is_action_just_pressed("disparar"):
+		disparar()
